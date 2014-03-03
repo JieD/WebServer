a47a81f51896b280e1f966cb49ebaa2fb6266b7a
JieD <jied314@mail.sfsu.edu>
2014-03-02 22:13:01 -0800

    finish resolve and script_aliased?

--- lib/resource.rb
+++ lib/resource.rb
@@ -7,5 +7,42 @@ module WebServer
       @conf = httpd_conf
       @mimes = mimes
     end
+   
+    # check uri of request to resolve aliased and script aliased issue
+    def resolve
+      uri = @request.uri
+      uri.match /(.*)\/(.*)/i
+      absolute_path = "#{@conf.document_root}"
+      
+      if aliased? # need to check aliased? first due to spec test
+        # puts $1, $2
+        # print "aliased: true\n"
+        # return format: 
+        # document_root + alias path + file + directory_index
+        absolute_path += "#{@conf.alias_path($1)}/#{$2}/#{@conf.directory_index}"
+      else
+        if script_aliased?
+          # puts $1, $2
+          # print "script_aliased: true\n"
+          # return format: document_root + script alias path + file
+           absolute_path += "#{@conf.script_alias_path($1)}/#{$2}"
+        else 
+          # puts $1, $2 
+          # print "non_aliased: true\n" 
+          # return format: document_root + uri + directory_indes
+          absolute_path += "#{uri}/#{@conf.directory_index}"
+        end
+      end
+    end
+
+    def script_aliased?
+      @request.uri.match /(.*)\/(.*)/i
+      @conf.script_aliases.include? $1 
+    end
+ 
+    def aliased?
+      @request.uri.match /(.*)\/(.*)/i
+      @conf.aliases.include? $1  
+    end
   end
 end
