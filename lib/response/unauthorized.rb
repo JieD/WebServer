211abddd33eb320754cd0cb01944d250900bdef8
JieD <JieD@users.noreply.github.com>
2014-03-05 21:21:14 -0800

    Update unauthorized.rb

--- lib/response/unauthorized.rb
+++ lib/response/unauthorized.rb
@@ -3,6 +3,7 @@ module WebServer
     # Class to handle 401 responses
     class Unauthorized < Base
       def initialize(resource, options={})
+        super(resource,options)
       end
     end
   end
