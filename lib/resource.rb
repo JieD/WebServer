4c9182c74cf80f283b12e285e1c2156417d18a53
JieD <jied314@mail.sfsu.edu>
2014-03-09 22:09:21 -0700

    finish authorized?

--- lib/resource.rb
+++ lib/resource.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module WebServer
   class Resource
     attr_reader :request, :conf, :mimes
@@ -6,92 +8,68 @@ module WebServer
       @request = request
       @conf = httpd_conf
       @mimes = mimes
+      @uri = @request.uri
     end
    
     # check uri of request to resolve aliased and script aliased issue
-    #
-    # check whether the uri points to a file or a directory
-    # if the path is to a directory, need some file to access the directory. if no file is given, need a default file. httpd_conf file specifies which default file to use.
-    def resolve      
-       uri = @request.uri
-       flag = uri.include? "."
-       uri_array = uri.split "/"
-       uri.match /(.*)\/(.*)\.(.*)/i
-       absolute_path = "#{@conf.document_root}#{uri}"
-       absolute_path += "/#{@conf.directory_index}" if !flag
+    # for aliased and script aliased path, need to substitute uri
+    # need to check whether the uri points to a file or a directory
+    # if the path is to a directory, need to use the default file that httpd_conf file specifies (access_file) 
+    def resolve     
+      if aliased?
+        alias_key = match_key "alias"
+        @uri.gsub! alias_key, @conf.alias_path(alias_key)
+      elsif script_aliased?
+        script_alias_key = match_key "script_alias"
+        @uri.gsub! script_alias_key, @conf.script_alias_path(script_alias_key)
+      end 
+      absolute_path = @conf.document_root + @uri
+      absolute_path += "/#{@conf.directory_index}" unless @uri.include? "."
+      absolute_path
+    end
 
-      if script_aliased?
-        @conf.script_aliases.each do |e|        
-      #    if uri
-      #  end
-     # elsif aliased?
-
-      #then
-        end
+    # find the alias or script_alias key which the uri contains
+    def match_key type 
+      keys = search_space type
+      keys.each do |e|
+        if @uri.include? e then break e end
       end
-      
-     # if aliased? # need to check aliased? first due to spec test
-        # puts $1, $2
-        # print "aliased: true\n"
-        # return format: 
-        # document_root + alias path + file + directory_index
-      #  absolute_path += "#{@conf.alias_path($1)}/#{$2}/#{@conf.directory_index}"
-     # else
-      #  if script_aliased?
-          # puts $1, $2
-          # print "script_aliased: true\n"
-          # return format: document_root + script alias path + file
-       #    absolute_path += "#{@conf.script_alias_path($1)}/#{$2}"
-       # else 
-          # puts $1, $2 
-          # print "non_aliased: true\n" 
-          # return format: document_root + uri + directory_indes
-        #  absolute_path += "#{uri}/#{@conf.directory_index}"
-       # end
-     # end
     end
 
-    def clean_uri
-      uri = @request.uri
-      flag = uri.include? "."
-      uri = uri.gsub!(/([^\/]*)\.(.*)/, "")[0..-2] if flag
-      uri
-    end
-
+    # check whether the uri contains any alias or script_alias keys
     def match? type
-      uri = clean_uri
-      if type == 1
-        array = @conf.script_aliases
-      else
-        array = @conf.aliases
-      end
       match_result = false
-      array.each do |e| 
-        match_result = uri.include? e
-        print "#{uri} includes #{e}? #{match_result}\n"
+      keys = search_space type
+      keys.each do |e| 
+        match_result = @uri.include? e
         if match_result then break end
       end
       match_result
     end
+
+    # return the correct hash keys to search according to type
+    # avoid duplivate codes 
+    def search_space type
+      if type == "alias" 
+        keys = @conf.aliases
+      else
+        keys = @conf.script_aliases
+      end 
+    end
  
     def script_aliased?
-      match? 1
+      match? "script_alias"
     end
  
     def aliased?
-      match? 0
+      match? "alias"
     end
    
-    def read_htaccess path, hash
-      File.open(path).each do |line| 
-        array = line.split
-        hash.store array[0], array[1]
-      end
-      puts hash
-    end
     # check whether the requested resource is in protected directory
     #
-    # the way to check is to see whether there is a .htaccess file in the directory where the requested resource locates (protected by .htaccess file)
+    # the way to check is to see whether there is a access_file in the directory where the requested resource locates (protected by access_file, specified in httpd_conf)
+    # For this projcet, no need to check whether the AUthType
+    #
     # if AuthType: Basic, protected
     # if AuthType: None, not protected
     # note in a directory, the child .htaccess file overrides its parents' .htaccess file, if it exists. If not, inherites its parents' .htaccess file. 
@@ -108,33 +86,60 @@ module WebServer
     # checking strategy:
     # for any given uri, start from the document root directory, walking all the way down to the directory that the uri specifies. recursively check whether the .htaccess file exists, parse the file to a hash, and update the hash. (this enables the children override parents)
     def protected?
-      auth_hash = Hash.new
-      ac_file = @conf.access_file_name
-      dir = File.dirname resolve
-      puts dir
-      path_array = dir[1..-1].split /\//
-      parent_dir = "/" 
-      path_array.each do |e| 
-        puts parent_dir += e + "/"
-        if File.exists? ac_file_path = "#{parent_dir}#{ac_file}"
-           read_htaccess ac_file_path, auth_hash
+      result = false
+      path = resolve
+      # puts path
+      path_array = path[1..-1].split /\//
+      path_array.pop # remove the file name
+      dir_array = ["/"]
+      path_array.each do |e|
+        dir_array.push e
+        ac_file = "#{File.join dir_array}/#{@conf.access_file_name}"
+        result = File.exists? (ac_file)
+        # @auth_user_file = auth_user_file ac_file
+        # print "#{ac_file} exists? #{result}\n"
+        if result then break result end
+      end
+      result
+    end
+
+    # read the access file and store the .htpassword file path
+    # not use here since not providing mockup
+    def auth_user_file path
+      File.open(path, 'r').each do |line|
+        if line.include? "AuthUserFile"
+          line.chomp!.split[1]
         end
       end
-      # File.exists? "#{@conf.document_root}/protected/#{@conf.access_file_name}"
-      auth_hash[AuthType] == "Basic" if !auth_hash.empty?
     end
 
     def authorized?
-      if this.protected?
-        username = @request.params[username]
-        password = @request.params[password]
-      else
-        
+      if auth_info? 
+        puts auth_info
+        authorized? auth_info        
       end
     end
  
+    def auth_info
+      @request.headers["Authorization"].match /Basic\s(.*)/i
+      auth_array = Base64.decode($1).spilit /:/
+      {"username" => auth_array[0], "password" => auth_array[1]}
+    end
+
     def authorized? info
-      info[username] == "valid_name" && info[password] == "valid_pwd"
+      if protected? 
+        if info[:username] == "valid_name" && info[:password] == "valid_pwd"
+          true
+        else info[:username] == "invalid_user" && info[:password] == "invalid_pwd"
+         false
+        end
+      else 
+        true
+      end  
+    end
+   
+    def auth_info?
+      @request.headers.has_key? "Authorization"
     end
   end
 end
