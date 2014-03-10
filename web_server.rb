bcd0dd5c24750c98bc3d5d6ae0224146aa2179b6
JieD <JieD@users.noreply.github.com>
2014-03-09 22:03:39 -0700

    Update web_server.rb

--- web_server.rb
+++ web_server.rb
@@ -10,26 +10,12 @@ module WebServer
     attr_accessor :conf, :mime
 
     def initialize(options={})
-      # Set up WebServer's configuration files and logger here
-      # Do any preparation necessary to allow threading multiple requests
-      @conf = WebServer::Httpd_conf.new
-      @mime = WebServer::Mimetypes.new
+      
     end
 
     def start
         
-      # Begin your 'infinite' loop, reading from the TCPServer, and
-      # processing the requests as connections are made
-      @logger.log_message="{SERVER} Starts on port #{conf.port}"
-
-      #creating a thread when socket opened
-      #create worker with socket
-      while true 
-        Thread.start(server.accept) do |client|
-          WebServer::worker.new client, # logger
-        Thread.current[:worker]=WebServer::Worker.new(client,self)
-        end
-      end
+      
     end
 
     def server
