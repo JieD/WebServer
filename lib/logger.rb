1717b1ad054836f6775e41a283172f85f16e91da
JieD <JieD@users.noreply.github.com>
2014-03-09 22:05:51 -0700

    Update logger.rb

--- lib/logger.rb
+++ lib/logger.rb
@@ -6,23 +6,12 @@ module WebServer
         # allow me to decide if I want my server to print out log
         # messages as they get generated
         def initialize(log_file_path, options={})
-            @path=log_file_path
-            logfile=@path+"\\log.txt"
-            $log=File.open(logfile, "w+")
+ 
         end
         
         # Log a message using the information from Request and
         # Response objects
-        def log(request, response)
-            
-            log_message="\n\n==========\n#{response.message}"
-            puts log_message
-            $log.puts log_message unless $log==nil
-            client=WebServer.accept
-            @request=threads.gets
-            log_message="#{client.peeraddr[2]} (#client.peeraddr[3]})\n"
-            #log_message+=Time.now.localtime.strftime("%Y/%m%d ％H：％M：％S")
-            log_message+="\n#{request}"
+        def log(request, response0
             
         end
         
@@ -30,7 +19,7 @@ module WebServer
         # Allow the consumer of this class to flush and close the
         # log file
         def close
-            $log=File.close(logfile,"w+")
+
         end
     end
 end
