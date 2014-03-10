e5cc1b0faa82e4393dfb0e46aa59ba60536ec02c
JieD <jied314@mail.sfsu.edu>
2014-03-09 22:08:43 -0700

    final;

--- lib/logger.rb
+++ lib/logger.rb
@@ -6,40 +6,23 @@ module WebServer
     # allow me to decide if I want my server to print out log
     # messages as they get generated
     def initialize(log_file_path, options={})
-
-        @path=log_file_path
-        @options={
-            'yes'=>'',
-            'no'=>'',
-        }
-        
-        options.each{|k,v|
-            k=k.to_s
-            raise ArgumentError, "Unknown option: #{k}" unless @options.option?(k)
-            @options[k]=v
-        }
+      @log_file = File.open log_file_path, 'w'
+      @log_no = 0;
     end
     
-    def [](options)
-        options=options.to_s
-        raise ArgumentError, "unknown option: #{options}" unless @options.option?(options)
-        @options[options]
-    end
-    
-    def [](options,val)
-        options=options.to_s
-        raise ArgumentError, "unknown option: #{options}" unless @options.option?(options)
-        @options[options]=val
-    end
-
     # Log a message using the information from Request and 
     # Response objects
     def log(request, response)
+      time = Time.new.strftime("%Y-%m-%d %H-%M-%S")
+      http_request = "#{request.http_method} #{request.uri}"
+      http_response = "#{response.code}"
+      @log_file.puts "#{++@log_no} -- [#{time}] #{http_request} #{http_response}"
     end
 
     # Allow the consumer of this class to flush and close the 
     # log file
     def close
+      @log_file.close
     end
   end
 end
